b9fded5911b0d61fc019ab513a0107946d292175
Joseph Wortmann <joseph.wortmann@gmail.com>
2023-12-02 21:49:45 +0000

    get the template bacj

--- analysis2template/__init__.py
+++ analysis2template/__init__.py
@@ -4,6 +4,22 @@ from boto3 import client
 from boto3.compat import filter_python_deprecation_warnings
 from logging import getLogger, ERROR
 import json
+from typing import TYPE_CHECKING
+from time import sleep
+from uuid import uuid4
+
+if TYPE_CHECKING:
+    from mypy_boto3_quicksight.type_defs import (
+        TemplateSourceEntityTypeDef,
+        TemplateSourceAnalysisTypeDef,
+        DataSetReferenceTypeDef,
+    )
+else:
+    DataSetReferenceTypeDef = dict
+    TemplateSourceAnalysisTypeDef = dict
+    TemplateSourceEntityTypeDef = dict
+
+
 
 filter_python_deprecation_warnings()
 
@@ -30,22 +46,65 @@ def analysis2template(
         aws_account_id = client("sts").get_caller_identity()["Account"]
         quicksight = client("quicksight")
         if analysis_name:
-            params = dict(
-                AwsAccountId=aws_account_id
-            )
+            params = dict(AwsAccountId=aws_account_id)
             while True:
-                response = quicksight.list_analyses(**params)
-                for analysis_summary in response["AnalysisSummaryList"]:
+                analyses = quicksight.list_analyses(**params)
+                for analysis_summary in analyses["AnalysisSummaryList"]:
                     if analysis_name == analysis_summary["Name"]:
                         analysis_id = analysis_summary["AnalysisId"]
                         break
-                if i or not response.get("NextToken"):
+                if i or not analyses.get("NextToken"):
                     break
-                params["NextToken"] = response["NextToken"]
+                params["NextToken"] = analyses["NextToken"]
             if not analysis_id:
                 raise CommandError(f"Analyis {n} not found")
-            response = quicksight.describe_analysis_definition(AwsAccountId=aws_account_id, AnalysisId=analysis_id)
-            yield json.dumps(response, indent=2)
+            analysis = quicksight.describe_analysis(
+                AwsAccountId=aws_account_id, AnalysisId=analysis_id
+            )
+            analysis_definition = quicksight.describe_analysis_definition(
+                AwsAccountId=aws_account_id, AnalysisId=analysis_id
+            )
+            template_id = str(uuid4())
+            quicksight.create_template(
+                AwsAccountId=aws_account_id,
+                TemplateId=template_id,
+                SourceEntity=TemplateSourceEntityTypeDef(
+                    SourceAnalysis=TemplateSourceAnalysisTypeDef(
+                        Arn=analysis["Analysis"]["Arn"],
+                        DataSetReferences=[
+                            DataSetReferenceTypeDef(
+                                DataSetPlaceholder=dataset["Identifier"],
+                                DataSetArn=dataset["DataSetArn"],
+                            )
+                            for dataset in analysis_definition["Definition"][
+                                "DataSetIdentifierDeclarations"
+                            ]
+                        ],
+                    )
+                ),
+            )
+            try:
+                while True:
+                    status = quicksight.describe_template(
+                        AwsAccountId=aws_account_id, TemplateId=template_id
+                    )["Template"]["Version"]["Status"]
+                    if status in ("CREATION_FAILED", "UPDATE_FAILED", "DELETED"):
+                        raise RuntimeError(
+                            f"Unable to create intermediate template: {status}"
+                        )
+                    if status in ("CREATION_IN_PROGRESS", "UPDATE_IN_PROGRESS"):
+                        sleep(5)
+                        continue
+                    break
+                template = quicksight.describe_template_definition(
+                    AwsAccountId=aws_account_id, TemplateId=template_id
+                )
+                yield json.dumps(template, indent=2)
+            finally:
+                quicksight.delete_template(
+                    AwsAccountId=aws_account_id, TemplateId=template_id
+                )
 
     except Exception as error:
         raise CommandError(error)
+
